c7de68d250e732593e3c0a602ceae97506e1847a
iangomes <wanderleygomesian@gmail.com>
2025-12-03 22:41:17 -0300

    Signed-off-by: iangomes <wanderleygomesian@gmail.com>
    # Carrinho funcionando na simulação para segunda parte do projeto final

--- codigo_carrinho.asm
+++ codigo_carrinho.asm
@@ -14,6 +14,8 @@ CBLOCK  0x70
     AT0         ; Contador de atraso nível 1
     AT1         ; Contador de atraso nível 2
     AT2         ; Contador de atraso nível 3
+    FLAG        ; Variável de flag para controle (bit0 = primeira vez 11, bit1 = segunda vez 11)
+    TEMP        ; Variável temporária
 ENDC
 
 CONT    EQU     0x20    ; Contador de estados para detecção de obstáculos
@@ -37,6 +39,9 @@ LED10      EQU 2        ; RA2
 LED11      EQU 3        ; RA3
 LED_FIM    EQU 4        ; RA4 (open-drain no 16F628A - atenção ao hardware)
 
+; Bits da variável FLAG
+PRIMEIRA_VEZ EQU 0      ; Bit 0 - primeira vez encontrou 11
+SEGUNDA_VEZ  EQU 1      ; Bit 1 - segunda vez encontrou 11
 
 ;=============================================================================
 ; Vetores
@@ -51,7 +56,6 @@ LED_FIM    EQU 4        ; RA4 (open-drain no 16F628A - atenção ao hardware)
 ; Inicialização
 ;=============================================================================
 INICIO:
-
         ; Configura TRISB: RB0-RB2 entradas, RB3-RB7 saídas
         BANKSEL TRISB
         MOVLW   b'00000111'     ; 1=entrada, 0=saida
@@ -76,12 +80,12 @@ INICIO:
         ; Inicializa variáveis
         CLRF    CONT
         CLRF    CURVA
+        CLRF    FLAG
 
 ;=============================================================================
 ; Loop Principal
 ;=============================================================================
 MAIN:
-
         ; --- 1) CONDIÇÃO DE PARADA: AMBOS SENSORES LATERAIS ATIVOS ---
         BANKSEL PORTB
         BTFSS   PORTB, S_ESQ       ; se ESQ=0, continua seguindo
@@ -98,13 +102,56 @@ PARAR:
         BCF     PORTB, M_REV_D
         GOTO    PARAR              ; trava parado (laço intencional)
 
-
 ;=============================================================================
-; LÓGICA PRINCIPAL DE SEGUIMENTO DE LINHA
+; LÓGICA PRINCIPAL DE SEGUIMENTO DE LINHA COM CONDIÇÃO ESPECIAL
 ;=============================================================================
 SEGUE:
         BANKSEL PORTB
+        
+        ; --- Primeiro verifica condição especial: sensor esquerdo e meio = 11 ---
+        ; Verifica se sensor esquerdo = 1 E sensor meio = 1
+        BTFSS   PORTB, S_ESQ       ; Verifica se esquerdo = 1
+        GOTO    LOGICA_NORMAL      ; Se não, vai para lógica normal
+        
+        BTFSS   PORTB, S_FRENTE    ; Verifica se meio = 1
+        GOTO    LOGICA_NORMAL      ; Se não, vai para lógica normal
+        
+        ; --- Se chegou aqui: ESQUERDO=1 e MEIO=1 (condição 11) ---
+        
+        ; Verifica se já foi primeira vez
+        BANKSEL FLAG
+        BTFSS   FLAG, PRIMEIRA_VEZ
+        GOTO    PRIMEIRA_VEZ_11    ; Primeira vez encontrando 11
+        
+        ; Se já foi primeira vez, verifica se é segunda vez
+        BTFSS   FLAG, SEGUNDA_VEZ
+        GOTO    SEGUNDA_VEZ_11     ; Segunda vez encontrando 11
+        
+        ; Se já foi segunda vez, continua parado
+        GOTO    PARAR
 
+PRIMEIRA_VEZ_11:
+        ; Marca que encontrou primeira vez
+        BANKSEL FLAG
+        BSF     FLAG, PRIMEIRA_VEZ
+        
+        ; Vai para ajuste esquerda e espera até ver apenas o meio
+        CALL    AJUSTE_ESPERA_MEIO
+        
+        ; Volta para MAIN
+        GOTO    MAIN
+
+SEGUNDA_VEZ_11:
+        ; Marca que encontrou segunda vez
+        BANKSEL FLAG
+        BSF     FLAG, SEGUNDA_VEZ
+        
+        ; Para permanentemente
+        GOTO    PARAR
+
+LOGICA_NORMAL:
+        BANKSEL PORTB
+        
         ; --- 2) Linha sob o sensor central → andar reto ---
         BTFSC   PORTB, S_FRENTE
         GOTO    ANDAR_RETO
@@ -121,8 +168,83 @@ PERDEU_LINHA:
         BANKSEL PORTB
         BCF     PORTB, M_E        ; desliga motor esquerdo
         BSF     PORTB, M_D        ; motor direito ligado → curva para direita
+        
+        ; Verifica se durante a curva encontra condição 11 novamente
+        CALL    VERIFICA_11_DURANTE_CURVA
         GOTO    MAIN
 
+;=============================================================================
+; Subrotina: Verifica condição 11 durante a curva
+;=============================================================================
+VERIFICA_11_DURANTE_CURVA:
+        BANKSEL PORTB
+        ; Verifica se sensor esquerdo = 1 E sensor meio = 1
+        BTFSS   PORTB, S_ESQ       ; Verifica se esquerdo = 1
+        RETLW   0                  ; Se não, retorna
+        
+        BTFSS   PORTB, S_FRENTE    ; Verifica se meio = 1
+        RETLW   0                  ; Se não, retorna
+        
+        ; Se encontrou 11 durante a curva
+        ; Verifica se já foi primeira vez
+        BANKSEL FLAG
+        BTFSS   FLAG, PRIMEIRA_VEZ
+        GOTO    MARCA_PRIMEIRA_VEZ
+        
+        ; Se já foi primeira vez, marca segunda vez
+        BSF     FLAG, SEGUNDA_VEZ
+        GOTO    PARAR
+        
+MARCA_PRIMEIRA_VEZ:
+        BSF     FLAG, PRIMEIRA_VEZ
+        RETLW   0
+
+;=============================================================================
+; Subrotina: Ajuste esquerda e espera até ver apenas o meio
+;=============================================================================
+AJUSTE_ESPERA_MEIO:
+        BANKSEL PORTB
+        
+        ; Ajusta para esquerda
+        BCF     PORTB, M_E
+        BSF     PORTB, M_D
+        BCF     PORTB, M_REV_E
+        BCF     PORTB, M_REV_D
+        
+        ; Pequeno delay para iniciar movimento
+        CALL    DELAY_PEQUENO
+        
+ESPERA_MEIO_LOOP:
+        ; Verifica se esquerdo = 0 e meio = 1
+        BTFSC   PORTB, S_ESQ      ; Se esquerdo = 1, continua ajustando
+        GOTO    CONTINUA_AJUSTE
+        
+        BTFSS   PORTB, S_FRENTE   ; Se meio = 0, continua ajustando
+        GOTO    CONTINUA_AJUSTE
+        
+        ; Quando chega aqui: esquerdo=0 e meio=1
+        ; Para os motores
+        BCF     PORTB, M_E
+        BCF     PORTB, M_D
+        
+        ; Delay para estabilização
+        CALL    DELAY_PEQUENO
+        
+        ; Verifica novamente para confirmar
+        BTFSC   PORTB, S_ESQ
+        GOTO    CONTINUA_AJUSTE
+        
+        BTFSS   PORTB, S_FRENTE
+        GOTO    CONTINUA_AJUSTE
+        
+        ; Confirmação ok, retorna
+        RETLW   0
+
+CONTINUA_AJUSTE:
+        ; Continua ajustando para esquerda
+        BCF     PORTB, M_E
+        BSF     PORTB, M_D
+        GOTO    ESPERA_MEIO_LOOP
 
 ;=============================================================================
 ; Movimentos
@@ -150,5 +272,39 @@ AJUSTE_DIR:
         BCF     PORTB, M_REV_E
         BCF     PORTB, M_REV_D
         GOTO    MAIN
+
+;=============================================================================
+; Subrotinas de Delay
+;=============================================================================
+DELAY_PEQUENO:
+        MOVLW   .50
+        MOVWF   AT1
+DELAY_P1:
+        MOVLW   .100
+        MOVWF   AT0
+DELAY_P0:
+        DECFSZ  AT0, F
+        GOTO    DELAY_P0
+        DECFSZ  AT1, F
+        GOTO    DELAY_P1
+        RETURN
+
+DELAY:
+        MOVLW   .100
+        MOVWF   AT2
+DELAY_LOOP2:
+        MOVLW   .100
+        MOVWF   AT1
+DELAY_LOOP1:
+        MOVLW   .10
+        MOVWF   AT0
+DELAY_LOOP0:
+        DECFSZ  AT0, F
+        GOTO    DELAY_LOOP0
+        DECFSZ  AT1, F
+        GOTO    DELAY_LOOP1
+        DECFSZ  AT2, F
+        GOTO    DELAY_LOOP2
+        RETURN
 
         END
